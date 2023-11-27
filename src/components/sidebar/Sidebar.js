34b4e3226b476701d0ec50026e32eef35a4e5e84
taiki0330 <jonas.taiki.0330@icloud.com>
2023-11-28 01:46:27 +0900

    first commit

---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import SidebarOptions from './SidebarOptions';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import HomeIcon from '@mui/icons-material/Home';
+import SearchIcon from '@mui/icons-material/Search';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
+import ListAltIcon from '@mui/icons-material/ListAlt';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import { Button } from '@mui/material';
+import "./Sidebar.css"
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      {/* ツイッターアイコン */}
+      <TwitterIcon className="sidebar_twitterIcon" />
+
+      {/* SidebarOption */}
+      <SidebarOptions text="ホーム" Icon={HomeIcon} />
+      <SidebarOptions text="話題を検索" Icon={SearchIcon} />
+      <SidebarOptions text="通知" Icon={NotificationsNoneIcon} />
+      <SidebarOptions text="メッセージ" Icon={MailOutlineIcon} />
+      <SidebarOptions text="ブックマーク" Icon={BookmarkBorderIcon} />
+      <SidebarOptions text="リスト" Icon={ListAltIcon} />
+      <SidebarOptions text="プロフィール" Icon={AccountCircleIcon} />
+      <SidebarOptions text="もっと見る" Icon={MoreHorizIcon} />
+
+      {/* ツイートボタン */}
+      <Button variant='outlined' className='sidebar_tweet'>ツイートする</Button>
+    </div>
+  );
+}
+
+export default Sidebar;
---
+++ src/components/sidebar/SidebarOptions.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./SidebarOptions.css"
+
+function SidebarOptions({ text, Icon }) {
+  return (
+    <div className='sidebarOptions'>
+        <Icon/>
+        <h2>{text}</h2>
+    </div>
+  );
+}
+
+export default SidebarOptions
